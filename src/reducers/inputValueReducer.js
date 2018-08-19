f8f3392c0c5ce9aa4cef3138adc061063da07d45
behzas ss <behzadspacey@gmail.com>
2018-08-19 11:33:48 +0430

    import Types from actionTypes and write function for inputValuechage

--- src/reducers/inputValueReducer.js
+++ src/reducers/inputValueReducer.js
@@ -1,4 +1,8 @@
+import { INPUT_VALUE_CHANGE } from '../actionTypes';
+
 export default function inputValueReducer(state = '', action) {
-  
+    if (action.type === INPUT_VALUE_CHANGE) {
+        return action.payload;
+      }
     return state;
   }
