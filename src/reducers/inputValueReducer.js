b814fb39a8f9309ff868837b09b6621f8ab46692
behzas ss <behzadspacey@gmail.com>
2018-08-19 11:56:39 +0430

    write function in both files for add new title in <li>

--- src/reducers/inputValueReducer.js
+++ src/reducers/inputValueReducer.js
@@ -1,8 +1,11 @@
-import { INPUT_VALUE_CHANGE } from '../actionTypes';
+import { INPUT_VALUE_CHANGE,TO_DO_ITEM} from '../actionTypes';
 
 export default function inputValueReducer(state = '', action) {
     if (action.type === INPUT_VALUE_CHANGE) {
         return action.payload;
+      }
+      if (action.type === TO_DO_ITEM) {
+        return '';
       }
     return state;
   }
--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -1,5 +1,9 @@
+import { TO_DO_ITEM } from '../actionTypes';
+
 export default function itemsReducer(state = [], action) {
  
-
+  if (action.type === TO_DO_ITEM) {
+    return [...state , {title:action.payload}];
+  }
   return state;
 }
