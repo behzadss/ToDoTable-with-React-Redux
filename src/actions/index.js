c2b5105ac20c741dd073ff69d17e1932ba24abb3
behzas ss <behzadspacey@gmail.com>
2018-08-19 11:54:24 +0430

    write function for add title in <li>

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
-import { INPUT_VALUE_CHANGE } from '../actionTypes';
+import { INPUT_VALUE_CHANGE, TO_DO_ITEM } from '../actionTypes';
 export function inputValueChange(value) {
     return {
       type: INPUT_VALUE_CHANGE,
       payload: value,
+    };
+  }
+  export function todoItem(title) {
+    return {
+      type: TO_DO_ITEM,
+      payload: title,
     };
   }
