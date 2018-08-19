c1aedd699ab7f57bc1d4b42ac37b33519d0602fd
behzas ss <behzadspacey@gmail.com>
2018-08-19 11:07:43 +0430

    define mapStateToProps for connect to state and put that in connect and write list and ul , li , input with map.array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-const ConnectWrapper= connect();
+function mapStateToProps(state) {
+  return {
+    items: state.items,
+    inputValue: state.inputValue,
+  };
+}
+
+const ConnectWrapper= connect(mapStateToProps);
 class App extends Component {
   render() {
+    const { items, inputValue } = this.props;
+
     return (
-      <div className="App">
-      
+      <div>
+        <ol>
+          {items.map((item, index) => (
+            <li key={index}></li>
+          ))}
+        </ol>
+        <input value={inputValue}  />
+        <button >Add</button>
       </div>
     );
   }
