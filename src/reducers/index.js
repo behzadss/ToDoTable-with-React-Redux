d1f70095cfc19562b44088fac30231518e48f83a
behzas ss <behzadspacey@gmail.com>
2018-08-21 11:22:46 +0430

    add redux-form and combined in reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import {reducer as formReducer} from 'redux-form'
 import newTodoItemReducer from './newTodoItemReducer';
 import itemsReducer from './itemsReducer';
 
 export default combineReducers({
   items: itemsReducer,
   newTodoItem: newTodoItemReducer,
+  form:formReducer,
 });
