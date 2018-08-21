5865d0408d58dfbc3c4b828e71cf09bb8c555cad
behzas ss <behzadspacey@gmail.com>
2018-08-21 10:41:32 +0430

    import material-pickers and date-fns from material-pickers

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store';
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider'
+import DateFnsUtils from 'material-ui-pickers/utils/date-fns-utils';
 
 ReactDOM.render(
 <Provider store={store}>
+<MuiPickersUtilsProvider utils={DateFnsUtils}>
 <App />
+</MuiPickersUtilsProvider>
 </Provider>, 
 document.getElementById('root'));
