5884d53c84682877b6fbd435bf89211692da4d35
behzas ss <behzadspacey@gmail.com>
2018-08-21 09:40:35 +0430

    define new Types for handleMoveUp & handleMoveDown & handle status with checkbox

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -3,4 +3,9 @@ export const TO_DO_ITEM = 'TO_DO_ITEM';
 export const TO_DO_CHECK_CHANGE = 'TO_DO_CHECK_CHANGE';
 export const NEW_TODO_ITEM_DEADLINE_CHANGE = 'NEW_TODO_ITEM_DEADLINE_CHANGE';
 export const TO_All_SELECT = 'TO_All_SELECT';
+export const SET_DONE = 'SET_DONE';
+export const SET_REMOVE = 'SET_REMOVE';
+export const MOVE_UP = 'MOVE_UP';
+export const MOVE_DOWN = 'MOVE_DOWN';
+
 
