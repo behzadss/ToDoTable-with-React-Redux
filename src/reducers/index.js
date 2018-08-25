752acc67e354647b8c668f69d06e9d9041cbd14e
behzas ss <behzadspacey@gmail.com>
2018-08-25 09:28:01 +0430

    deleted items syntax and file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import {reducer as formReducer} from 'redux-form'
-import newTodoItemReducer from './newTodoItemReducer';
 import itemsReducer from './itemsReducer';
 
 export default combineReducers({
   items: itemsReducer,
-  newTodoItem: newTodoItemReducer,
   form:formReducer,
 });
